43d7ef01a40512157ee7de909cdae42861756ada
devinMcArthur <assortedgerm@gmail.com>
2024-06-26 18:57:58 -0600

    Actually make MaterialReportClass.jobsiteMaterial nullable

--- server/src/models/JobsiteDayReport/schema/subDocument.ts
+++ server/src/models/JobsiteDayReport/schema/subDocument.ts
@@ -81,7 +81,7 @@ export class MaterialReportClass {
   @Field(() => ID, { nullable: false })
   public _id?: Types.ObjectId;
 
-  @Field(() => JobsiteMaterialClass)
+  @Field(() => JobsiteMaterialClass, { nullable: true })
   @prop({ ref: () => JobsiteMaterialClass, required: true })
   public jobsiteMaterial!: Ref<JobsiteMaterialClass>;
 
